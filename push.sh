4eec346775f493aca2e49877e558316b4fb61834
Harsh Patel <Patelharsh241999@gmail.com>
2023-06-28 12:19:00 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
-
 if [[ -n "$1" ]]; then
-  commitMessage="$1"
+commitMessage="$1"
 else
-  read -p "Enter the commit message: " commitMessage
+read -p "Enter the commit message: " commitMessage
 fi
 
+for (( i=2; i<=$#; i++ )); do
+commitMessage="$commitMessage ${!i}"
+done
+
 git add .
-git commit -m "* $commitMessage"
+git commit -m "* ${commitMessage^} ."
 git push
+
+echo "Successfully committed with this message: ${commitMessage^}"
